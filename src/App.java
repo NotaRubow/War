5d98d635c24c04cadf8e9aba5622eef8ede6ce84
rubown_wlhs <rubown@wlhs.wlwv.k12.or.us>
2025-05-27 14:27:51 -0700

    Done for today

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ package src;
 
 public class App {
     public static void main(String[] args) {
-        Hangman.main(args);  // Launch the Hangman game
+        new Hangman();
     }
 }
--- src/Hangman.java
+++ src/Hangman.java
@@ -1,55 +1,102 @@
 package src;
 
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class Hangman {
-    public static void main(String[] args) {
-        String word = "java";
-        char[] guessed = new char[word.length()];
-        for (int i = 0; i < guessed.length; i++) {
-            guessed[i] = '_';
-        }
+public class Hangman extends JFrame {
+    private String word = "java";
+    private char[] guessed = {'_', '_', '_', '_'};
+    private int attemptsLeft = 6;
 
-        int attemptsLeft = 6;
-        Scanner scanner = new Scanner(System.in);
-        boolean wordGuessed = false;
+    private JLabel wordLabel = new JLabel(String.valueOf(guessed));
+    private JTextField guessField = new JTextField(1);
+    private HangmanPanel hangmanPanel = new HangmanPanel();
 
-        while (attemptsLeft > 0 && !wordGuessed) {
-            System.out.println("Word: " + String.valueOf(guessed));
-            System.out.println("Attempts left: " + attemptsLeft);
-            System.out.print("Guess a letter: ");
-            char guess = scanner.next().toLowerCase().charAt(0);
+    public Hangman() {
+        setTitle("Hangman Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setLayout(new BorderLayout());
 
-            boolean correctGuess = false;
-            for (int i = 0; i < word.length(); i++) {
-                if (word.charAt(i) == guess && guessed[i] == '_') {
-                    guessed[i] = guess;
-                    correctGuess = true;
-                }
+        JPanel topPanel = new JPanel();
+        topPanel.add(new JLabel("Guess the word: "));
+        topPanel.add(wordLabel);
+        add(topPanel, BorderLayout.NORTH);
+
+        JPanel inputPanel = new JPanel();
+        JButton guessButton = new JButton("Guess");
+        inputPanel.add(new JLabel("Enter a letter:"));
+        inputPanel.add(guessField);
+        inputPanel.add(guessButton);
+        add(inputPanel, BorderLayout.SOUTH);
+
+        add(hangmanPanel, BorderLayout.CENTER);
+
+        guessButton.addActionListener(e -> {
+            if (guessField.getText().length() > 0) {
+                char guess = Character.toLowerCase(guessField.getText().charAt(0));
+                guessField.setText("");
+                processGuess(guess);
             }
+        });
 
-            if (!correctGuess) {
-                attemptsLeft--;
-                System.out.println("Wrong guess!");
-            } else {
-                System.out.println("Good guess!");
-            }
-
-            wordGuessed = true;
-            for (char c : guessed) {
-                if (c == '_') {
-                    wordGuessed = false;
-                    break;
-                }
-            }
-            System.out.println();
-        }
-
-        if (wordGuessed) {
-            System.out.println("Congratulations! You guessed the word: " + word);
-        } else {
-            System.out.println("Game over! The word was: " + word);
-        }
-        scanner.close();
+        setVisible(true);
     }
+
+    private void processGuess(char guess) {
+        boolean correct = false;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess && guessed[i] == '_') {
+                guessed[i] = guess;
+                correct = true;
+            }
+        }
+
+        if (!correct) {
+            attemptsLeft--;
+        }
+
+        wordLabel.setText(String.valueOf(guessed));
+        hangmanPanel.setAttemptsLeft(attemptsLeft);
+
+        if (String.valueOf(guessed).equals(word)) {
+            JOptionPane.showMessageDialog(this, "You won! The word was: " + word);
+            System.exit(0);
+        } else if (attemptsLeft == 0) {
+            JOptionPane.showMessageDialog(this, "Game over! The word was: " + word);
+            System.exit(0);
+        }
+    }
+    class HangmanPanel extends JPanel {
+        private int attemptsLeft = 6;
+
+        public void setAttemptsLeft(int attemptsLeft) {
+            this.attemptsLeft = attemptsLeft;
+            repaint();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawLine(50, 250, 150, 250);
+            g.drawLine(100, 250, 100, 50);
+            g.drawLine(100, 50, 180, 50);
+            g.drawLine(180, 50, 180, 70);
+
+            if (attemptsLeft <= 5)
+                g.drawOval(160, 70, 40, 40);
+            if (attemptsLeft <= 4)
+                g.drawLine(180, 110, 180, 170);
+            if (attemptsLeft <= 3)
+                g.drawLine(180, 120, 150, 150);
+            if (attemptsLeft <= 2)
+                g.drawLine(180, 120, 210, 150);
+            if (attemptsLeft <= 1)
+                g.drawLine(180, 170, 150, 200);
+            if (attemptsLeft <= 0)
+                g.drawLine(180, 170, 210, 200);
+        }
+    }
+
 }
